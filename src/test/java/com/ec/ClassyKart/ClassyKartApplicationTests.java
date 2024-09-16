96bb9f4a9af8eafcc517b540ae362a64857a5269
eshwar <eshwar.choudhary@customerxps.com>
2024-09-16 21:46:12 +0530

    initial commit

---
+++ src/test/java/com/ec/ClassyKart/ClassyKartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ec.ClassyKart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassyKartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
